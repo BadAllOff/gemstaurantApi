18e28169d3cb16763f4b817c7be8ce747da74b92
Shamil <badalloffsh@gmail.com>
2017-12-03 22:40:33 +0100

    Add: order items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :orders, only: [:index]
 
   resources :tables do
-    resources :orders, only: [:create]
+    resources :orders, only: [:create] do
+      post :add, on: :member
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
