fb03a8ea6c2213e4ce856b54c529baa2d35aa7cf
Shamil <badalloffsh@gmail.com>
2017-12-03 17:39:24 +0100

    Add: Orders scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :tables
+  resources :orders, only: [:index]
+
+  resources :tables do
+    resources :orders, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
